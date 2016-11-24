b60d8204283f71e4eee31b199cf4d295159dc101
Harsh Chaturvedi <harsh.c@jhu.edu>
2016-11-24 15:47:13 -0500

    working script with right features

--- data_extraction/database.sql
+++ data_extraction/database.sql
@@ -1,3 +1,7 @@
 DROP TABLE IF EXISTS subject_dn ; 
-CREATE TABLE cert_data (sha256 TEXT, content TEXT, subject_c TEXT, subject_o TEXT, subject_cn TEXT, issuer_c TEXT, issuer_o TEXT, signing_algorithm TEXT, self_signed BOOL, key_algorithm TEXT, start_data TEXT, validity_length TEXT, org TEXT, common_name TEXT, issuer_org TEXT, issuer_country TEXT, issuer_common_name TEXT, subject_key_info TEXT, ip TEXT, sub_country TEXT, key_usage TEXT) ;
+
+CREATE TABLE subject_dn (sha256 TEXT, content TEXT, dns_names_count INT, subject_c TEXT, subject_ou TEXT, subject_o TEXT, subject_cn TEXT) ;
+CREATE TABLE dns_names (sha256 TEXT, content TEXT) ;
+CREATE TABLE issuer_dn (sha256 TEXT, content TEXT, issuer_c TEXT, issuer_ou TEXT, issuer_o TEXT, issuer_cn TEXT) ;
+CREATE TABLE cert_data (sha256 TEXT, content TEXT, subject_c TEXT, subject_o TEXT, subject_cn TEXT, issuer_c TEXT, issuer_o TEXT, signing_algorithm TEXT, self_signed BOOL, key_algorithm TEXT, val_start, val_length INT, ip TEXT, enc_only BOOL,cert_sign BOOL, key_enc BOOL, digi_sign BOOL, cont_commit BOOL,dec_only BOOL, key_agreem BOOL, data_enc BOOL) ;
 
